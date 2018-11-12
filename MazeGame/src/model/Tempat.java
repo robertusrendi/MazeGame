e09dd360a24ecff1c19cc30f13d13d85361e0700
ASUS <ASUS@DESKTOP-QAAS57R>
2018-11-12 23:39:32 +0700

    Verse 0.4

--- MazeGame/src/model/Tempat.java
+++ MazeGame/src/model/Tempat.java
@@ -5,20 +5,21 @@
  */
 package model;
 
+import java.awt.Color;
 import java.io.File;
 import java.util.ArrayList;
 
 /**
  *
- * @author jarkom
+ * @author user only
  */
 public class Tempat {
 
-    private String isi;
-    private int tinggi;
-    private int lebar;
-    
-    private ArrayList<Sel> daftarSel; 
+    private int tinggi; // tinggi tempat Game
+    private int lebar;  // lebar tempat Game
+    private ArrayList<Sel> daftarSel; // daftar sel
+
+    private String isi; // isi file konfigurasi
 
     public static int batasKanan;
     public static int batasBawah;
@@ -26,25 +27,104 @@ public class Tempat {
     public Tempat() {
         daftarSel = new ArrayList<Sel>();
     }
-    
-    public void BacaKonfigurasi(File file) {
+
+    /**
+     * Fungsi pembaca file konfigurasi. Hasil pembacaan file akan disimpan di
+     * atribut 'isi' dan juga di atribut daftarSel
+     *
+     * @param file
+     */
+    public void bacaKonfigurasi(File file) {
+
     }
 
-    public int hitungJumlahKress() {
+    /**
+     * Fungsi penambah daftar sel.
+     *
+     * @param sel
+     */
+    public void tambahSel(Sel sel) {
+        daftarSel.add(sel);
+    }
+
+    /**
+     * @return the tinggi
+     */
+    public int getTinggi() {
+        int tinggi = 0;
+        int i = 0;
+        int j = this.getIsi().length() - 1;
+        while (i < this.getIsi().length()) {
+            if (i == j) {
+                tinggi++;
+                i++;
+            }
+            while (i < this.getIsi().length() && this.getIsi().charAt(i) != '\n' && i < j) {
+                i++;
+            }
+            while (i < this.getIsi().length() && this.getIsi().charAt(i) == '\n' && i < j) {
+                i++;
+            }
+        }
+        return tinggi;
+    }
+
+    /**
+     * @param tinggi the tinggi to set
+     */
+    public void setTinggi(int tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    /**
+     * @return the lebar
+     */
+    public int getLebar() {
+        int lebar = 0;
+        int i = 0;
+        while (i < this.getIsi().length()) {
+            while (i < this.getIsi().length() && this.getIsi().charAt(i) != '\n') {
+                i++;
+            }
+            if (i<this.getIsi().length() && this.getIsi().charAt(i)=='\n') {
+                return lebar = i;
+            }
+        }
         return 0;
     }
 
-    public int jumlah0() {
-        return 0;
+    /**
+     * @param lebar the lebar to set
+     */
+    public void setLebar(int lebar) {
+        this.lebar = lebar;
     }
 
-    public int jumlahDot() {
-        return 0;
+    /**
+     * @return the daftarSel
+     */
+    public ArrayList<Sel> getDaftarSel() {
+        return daftarSel;
     }
 
-    public void hitungLebar() {
+    /**
+     * @param daftarSel the daftarSel to set
+     */
+    public void setDaftarSel(ArrayList<Sel> daftarSel) {
+        this.daftarSel = daftarSel;
     }
 
-    public void hitungTinggi() {
+    /**
+     * @return the isi
+     */
+    public String getIsi() {
+        return isi;
+    }
+
+    /**
+     * @param isi the isi to set
+     */
+    public void setIsi(String isi) {
+        this.isi = isi;
     }
 }
